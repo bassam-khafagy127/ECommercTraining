d1abb2a34377aad006d583fcdfbecee5e8544e6d
bassamkhafagy <bassamkhafgy127@gmail.com>
2023-05-25 03:12:57 +0300

    initial commit1

---
+++ app/src/main/java/com/example/ecommerc_training/activites/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerc_training.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecommerc_training.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
---
+++ app/src/main/java/com/example/ecommerc_training/activites/ShoppingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerc_training.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecommerc_training.R
+
+class ShoppingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
